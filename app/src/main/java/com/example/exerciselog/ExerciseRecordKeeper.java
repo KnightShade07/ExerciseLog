dc35b3bf3a033993acdfa7f4f426ffdac2a08407
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-12-05 15:38:56 -0800

    Made Log Text Box ReadOnly, gave name to header boz.

--- app/src/main/java/com/example/exerciselog/ExerciseRecordKeeper.java
+++ app/src/main/java/com/example/exerciselog/ExerciseRecordKeeper.java
@@ -12,8 +12,8 @@ public class ExerciseRecordKeeper extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_record_keeper);
-
         returnButton();
+
     }
     private void returnButton(){
         Button returnButton = findViewById(R.id.btnDBEntry);
