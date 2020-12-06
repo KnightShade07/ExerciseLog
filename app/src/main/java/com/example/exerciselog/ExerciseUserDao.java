2260fbf77f0a96738199ac6817395cce2b498c54
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-12-05 17:57:51 -0800

    Added Dao Annotation to Dao Class

--- app/src/main/java/com/example/exerciselog/ExerciseUserDao.java
+++ app/src/main/java/com/example/exerciselog/ExerciseUserDao.java
@@ -1,11 +1,12 @@
 package com.example.exerciselog;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
 
 import java.util.List;
-
+@Dao
 public interface ExerciseUserDao {
     @Query("SELECT * FROM exerciseuser")
     List<ExerciseUser> getAll();
