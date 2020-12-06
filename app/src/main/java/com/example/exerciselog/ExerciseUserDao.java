cf4f0086c88fc041bc02d624bb97349264cb3731
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-12-05 16:54:13 -0800

    Added basic DB Queries

--- app/src/main/java/com/example/exerciselog/ExerciseUserDao.java
+++ app/src/main/java/com/example/exerciselog/ExerciseUserDao.java
@@ -1,5 +1,18 @@
 package com.example.exerciselog;
 
-public interface ExerciseUserDao {
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
 
+import java.util.List;
+
+public interface ExerciseUserDao {
+    @Query("SELECT * FROM exerciseuser")
+    List<ExerciseUser> getAll();
+
+    @Insert
+    void insertAll(ExerciseUser users);
+
+    @Delete
+    void delete(ExerciseUser user);
 }
