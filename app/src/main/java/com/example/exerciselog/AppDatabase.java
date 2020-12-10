4353c363737ed99a473743a00b5136ff31e61f9e
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-12-09 18:17:35 -0800

    Added exportschema, set to false

--- app/src/main/java/com/example/exerciselog/AppDatabase.java
+++ app/src/main/java/com/example/exerciselog/AppDatabase.java
@@ -3,7 +3,7 @@ package com.example.exerciselog;
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {ExerciseUser.class}, version = 1)
+@Database(entities = {ExerciseUser.class}, version = 1, exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract ExerciseUserDao exerciseUserDao();
 }
