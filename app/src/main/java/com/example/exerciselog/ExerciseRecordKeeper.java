137e54b90b4a07b1800f36ebf2864b4741be937c
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-12-10 12:51:48 -0800

    Moved returnButton method call to the bottom

--- app/src/main/java/com/example/exerciselog/ExerciseRecordKeeper.java
+++ app/src/main/java/com/example/exerciselog/ExerciseRecordKeeper.java
@@ -14,9 +14,9 @@ public class ExerciseRecordKeeper extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_record_keeper);
-        returnButton();
         TextView txtExerciseLog =  findViewById(R.id.txtExerciseLog);
         txtExerciseLog.setFocusable(false);
+        returnButton();
 
 
     }
