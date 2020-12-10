563f2c739ac41af4555f91a40f05e82197d40c7c
KnightShade07 <45324806+KnightShade07@users.noreply.github.com>
2020-12-09 20:17:05 -0800

    Added ExerciseRepository.java class body

--- app/src/main/java/com/example/exerciselog/ExerciseRepository.java
+++ app/src/main/java/com/example/exerciselog/ExerciseRepository.java
@@ -1,4 +1,24 @@
 package com.example.exerciselog;
 
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
 public class ExerciseRepository {
+    private ExerciseUserDao exerciseDao;
+    private LiveData<List<ExerciseUser>> allExercises;
+
+    ExerciseRepository(Application application) {
+        AppDatabase db = AppDatabase.getDatabase(application);
+        exerciseDao = db.exerciseUserDao();
+        allExercises = (LiveData<List<ExerciseUser>>) exerciseDao.getAll();
+    }
+
+    void insert(ExerciseUser exerciseUser) {
+        AppDatabase.databaseWriteExecutor.execute(() -> {
+            exerciseDao.insert();
+        });
+    }
 }
